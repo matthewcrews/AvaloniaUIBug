64f2abb4c27ae9cc3ce702c977a0f0fd04655baa
Matthew Crews <matthew@crews.email>
2023-03-23 18:10:34 -0700

    Update FileSelect.fs

--- FileSelect.fs
+++ FileSelect.fs
@@ -53,10 +53,10 @@ let handleFileSelect (window: Window, state: IWritable<State>) =
     }
     |> Async.Start
 
-let view (window: Window) =
+let scenarioSelection (window: Window, state: IWritable<State>) =
 
-    Component(fun ctx ->
-        let state = ctx.useState State.init
+    Component.create ("scenario_select", fun ctx ->
+        let state = ctx.usePassed state
         let logs = ctx.useState List.empty<string>
 
         Border.create [
@@ -83,42 +83,45 @@ let view (window: Window) =
                             | Scenario.Baseline ->
                                 CheckBox.create [
                                     CheckBox.content "Baseline"
-                                    CheckBox.isChecked true
-                                    CheckBox.onClick (fun _ ->
+                                    CheckBox.isChecked (state.Current.Scenario = Scenario.Baseline)
+                                    CheckBox.onClick ((fun _ ->
                                         let newState =
                                             { state.Current with
                                                 Scenario = Scenario.Baseline }
-                                        state.Set newState)
+                                        state.Set newState), SubPatchOptions.Always)
                                 ]
 
                                 CheckBox.create [
                                     CheckBox.content "Expand"
-                                    CheckBox.isChecked false
-                                    CheckBox.onClick (fun _ ->
+                                    CheckBox.isChecked (
+                                        match state.Current.Scenario with
+                                        | Scenario.Expand _ -> true
+                                        | _ -> false)
+                                    CheckBox.onClick ((fun _ ->
                                         let newState =
                                             { state.Current with
                                                 Scenario = Scenario.Expand (None, []) }
-                                        state.Set newState)
+                                        state.Set newState), SubPatchOptions.Always)
                                 ]
 
                             | Scenario.Expand (selectedOption, excludedOptions) ->
                                 CheckBox.create [
                                     CheckBox.content "Baseline"
                                     CheckBox.isChecked false
-                                    CheckBox.onClick (fun _ ->
+                                    CheckBox.onClick ((fun _ ->
                                         let newState =
                                             { state.Current with
                                                 Scenario = Scenario.Baseline }
-                                        state.Set newState)
+                                        state.Set newState), SubPatchOptions.Always)
                                 ]
                                 CheckBox.create [
                                     CheckBox.content "Expand"
                                     CheckBox.isChecked true
-                                    CheckBox.onClick (fun _ ->
+                                    CheckBox.onClick ((fun _ ->
                                         let newState =
                                             { state.Current with
                                                 Scenario = Scenario.Expand (None, []) }
-                                        state.Set newState)
+                                        state.Set newState), SubPatchOptions.Always)
                                 ]
 
                                 TextBlock.create [ TextBlock.text "Expand Selection" ]
@@ -129,22 +132,22 @@ let view (window: Window) =
                                         CheckBox.create [
                                             CheckBox.content op
                                             CheckBox.isChecked false
-                                            CheckBox.onClick (fun _ ->
+                                            CheckBox.onClick ((fun _ ->
                                                 let newState =
                                                     { state.Current with
                                                         Scenario = Scenario.Expand (Some op, []) }
-                                                state.Set newState)
+                                                state.Set newState), SubPatchOptions.Always)
                                         ]
 
                                 | Some selectedOption ->
                                     CheckBox.create [
                                         CheckBox.content selectedOption
                                         CheckBox.isChecked true
-                                        CheckBox.onClick (fun _ ->
+                                        CheckBox.onClick ((fun _ ->
                                             let newState =
                                                 { state.Current with
                                                     Scenario = Scenario.Expand (None, []) }
-                                            state.Set newState)
+                                            state.Set newState), SubPatchOptions.Always)
                                     ]
                                     TextBlock.create [ TextBlock.text "Exclude Selection" ]
                                     let nonSelectedOptions =
@@ -156,7 +159,7 @@ let view (window: Window) =
                                         CheckBox.create [
                                             CheckBox.content nonSelectedOption
                                             CheckBox.isChecked isSelected
-                                            CheckBox.onClick (fun _ ->
+                                            CheckBox.onClick ((fun _ ->
                                                 let newExcludedOptions =
                                                     if isSelected then
                                                         excludedOptions
@@ -166,7 +169,7 @@ let view (window: Window) =
                                                 let newState =
                                                     { state.Current with
                                                         Scenario = Scenario.Expand (Some selectedOption, newExcludedOptions) }
-                                                state.Set newState)
+                                                state.Set newState), SubPatchOptions.Always)
                                         ]
 
                             Button.create [
@@ -177,3 +180,10 @@ let view (window: Window) =
                     ]
                 ])
         ])
+
+
+let view (window: Window) =
+    Component (fun ctx ->
+        let state = ctx.useState State.init
+        scenarioSelection (window, state)
+    )
